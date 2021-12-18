9a86164cda7f02cfe27d544e896f53cbe9db3f54
yooseek <y1212ks@naver.com>
2021-12-18 14:16:27 +0900

    등록,수정,삭제

--- src/pages/book/updateForm.js
+++ src/pages/book/updateForm.js
@@ -1,10 +1,84 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Button, Form } from 'react-bootstrap';
 
-const UpdateForm = () => {
+const UpdateForm = (props) => {
+  const id = props.match.params.id;
+
+  const [book, setBook] = useState({
+    title: '',
+    author: '',
+  });
+  const changeValue = (e) => {
+    setBook({
+      ...book,
+      [e.target.name]: e.target.value,
+    });
+  };
+  useEffect(() => {
+    fetch('http://localhost:8080/book/' + id, {
+      method: 'GET',
+    })
+      .then((res) => res.json())
+      .then((res) => {
+        setBook(res);
+      });
+  }, []);
+  const submitBook = (e) => {
+    e.preventDefault(); // submit이 액션을 안타고 자기 할일을 그만함.
+    fetch('http://localhost:8080/book/' + id, {
+      method: 'put',
+      headers: {
+        'Content-Type': 'application/json; charset=utf-8',
+      },
+      body: JSON.stringify(book),
+    })
+      .then((res) => {
+        console.log(res);
+        if (res.status === 200) {
+          return res.json;
+        } else {
+          return null;
+        }
+      })
+      .then((res) => {
+        if (res !== null) {
+          props.history.push('/book/' + id);
+        } else {
+          // 원래는 catch로 처리하는게 좋음
+          alert('책 등록에 실패하셨습니다.');
+        }
+        console.log(res);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
   return (
-    <div>
-      <h1>수정 폼</h1>
-    </div>
+    <Form onSubmit={submitBook}>
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>Title</Form.Label>
+        <Form.Control
+          type="text"
+          placeholder="Enter Title"
+          onChange={changeValue}
+          name="title"
+          value={book.title}
+        />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>Author</Form.Label>
+        <Form.Control
+          type="text"
+          placeholder="Enter Author"
+          onChange={changeValue}
+          name="author"
+          value={book.author}
+        />
+      </Form.Group>
+      <Button variant="primary" type="submit">
+        Submit
+      </Button>
+    </Form>
   );
 };
 
